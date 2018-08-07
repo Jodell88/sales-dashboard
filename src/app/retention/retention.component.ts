0676297ff20863e3aa58896f500f5cc0cac2a39b
Nicholas De Souza <nd@example.com>
2018-08-07 00:04:10 -0400

    Finished retention form

--- src/app/retention/retention.component.ts
+++ src/app/retention/retention.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+export class RetentionModel {
+  constructor(
+    public agent?: string,
+    public custName?: string,
+    public accountNumber?: string,
+    public reason?: string
+  ) {}
+}
+
+
 @Component({
   selector: 'app-retention',
   templateUrl: './retention.component.html',
   styleUrls: ['./retention.component.css']
 })
 export class RetentionComponent implements OnInit {
+
+  title = 'Retention';
+
+  model = new RetentionModel();
+
+  reset() {
+    this.model = new RetentionModel();
+  }
 
   constructor() { }
 
