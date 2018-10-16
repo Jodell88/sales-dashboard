78fb21f9c0516e08f385b6f7df4b279b5fa58a21
Nicholas De Souza <nd@example.com>
2018-10-15 23:28:47 -0400

    beginning of upgrade downgrade case

--- src/app/upgrade-downgrade/upgrade-downgrade.component.ts
+++ src/app/upgrade-downgrade/upgrade-downgrade.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+export class UpgradeDowngrade {
+  constructor(
+    public oldPackage?: string,
+    public newPackage?: string,
+    public crm?: string
+  ) {}
+}
+
 @Component({
   selector: 'app-upgrade-downgrade',
   templateUrl: './upgrade-downgrade.component.html',
   styleUrls: ['./upgrade-downgrade.component.css']
 })
 export class UpgradeDowngradeComponent implements OnInit {
+
+  title = 'Voice Upgrade / Downgrade';
+
+    options = [
+    {value: 'No'},
+    {value: 'Yes'}
+  ];
+
+  model = new UpgradeDowngrade();
+
+  reset(){
+    this.model = new UpgradeDowngrade();
+  }
 
   constructor() { }
 
